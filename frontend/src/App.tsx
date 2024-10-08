0a7dc7eb1c605d67f519a463cf50143c6d97dc0e
Fardowsa I Abdalla <fardowsaabdalla14@gmail.com>
2024-10-08 18:28:02 -0400

    fixed isolated modules error and added home and products.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import Home from "./Home/Home";
+import Products from "./Products/Product";
 function App() {
   const openMenu = () => {
     document.querySelector(".sidebar")?.classList.add("open");
@@ -10,17 +12,18 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
   return (
+    <Router>
     <div className="grid-container">
-        <header className="header">
+        <div className="header">
             <div className="brand">
                 <button onClick={openMenu}>&#9776;</button>
                 <a href="index.html">Desert Banjo</a>
             </div>
             <div className="header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <Link to="/">Home</Link>
+                <Link to="/catalog">Catalog</Link>
             </div>
-        </header>
+        </div>
 
         <aside className="sidebar">
             <h3>Shopping Categories</h3>
@@ -31,72 +34,18 @@ function App() {
             </ul>
         </aside>
 
-        <main>
-            <div className="content">
-                <ul className="products">
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="Product"/>
-                            <div className="product-name">
-                                <a href="product.html"> Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="Product"/>
-                            <div className="product-name">
-                                <a href="product.html"> Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="Product"/>
-                            <div className="product-name">
-                                <a href="product.html"> Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="Product"/>
-                            <div className="product-name">
-                                <a href="product.html"> Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 reviews)</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                            <img className="product-image" src="images/d1.jpg" alt="Product"/>
-                            <div className="product-name">
-                                <a href="product.html"> Slim Shirt</a>
-                            </div>
-                            <div className="product-brand">Nike</div>
-                            <div className="product-price">$60</div>
-                            <div className="product-rating">4.5 Stars (10 reviews)</div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        <main className ="main">
+            <Router>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/catalog" element={<Products/>}/>
+            </Router>
         </main>
 
         <footer>
             &copy; 2022 Desert Banjo
         </footer>
     </div>
+    </Router>
   );
 }
 
